013ddac6c2ba0ae1f3335bc398a3bf928a5f4ff3
Mythical714 <160852611+Mythical714@users.noreply.github.com>
2024-06-10 08:48:55 +0200

    design updated
    added two textViews containing info about the student and the name of the app, also added a functional button that moves to the second screen

--- app/src/main/java/com/example/imadpracticum/MainActivity.kt
+++ app/src/main/java/com/example/imadpracticum/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.imadpracticum
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weatherButton=findViewById<Button>(R.id.weatherButton)
+        val userInfo=findViewById<TextView>(R.id.userInfo)
+
+        weatherButton.setOnClickListener(){ //(IIE,2024)
+            val intent= Intent(this,mainScreen::class.java)//(IIE,2024)
+            intent.putExtra("mainScreen",userInfo.text.toString())//(IIE,2024)
+            startActivity(intent)//(IIE,2024)
     }
 }
---
+++ app/src/main/java/com/example/imadpracticum/mainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.imadpracticum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
